5fd6eb790fc32c872e61c37a47039ffd5ca9e514
Bruno Staine <brunostaine@hotmail.com>
2022-02-02 14:55:19 -0300

    atualizações e página de css criada

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+// import React from 'react';
+import { FiSearch } from "react-icons/fi";
+import './style.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">Buscador de CEP</h1>
+
+      <div className="containerInput">
+        <input
+        type="text"
+        placeholder="Digite seu cep..."
+         />
+
+         <button className="buttonSearch">
+            <FiSearch size={25} color="#fff"/>
+         </button>
+      </div>
+
+      <main>
+        <h2>CEP: 79001123</h2>
+
+        <span>Rua de teste</span>
+        <span>Complemento: não tem</span>
+        <span>jardin bonito</span>
+        <span>Brasília - DF</span>
+      </main>
+      
+    </div>
+
+  );
+}
+
+export default App;
+
